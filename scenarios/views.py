7b37d483a087940e3c37d931c54b81be6bbb7f17
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2025-09-18 22:29:23 +0200

    Add scenarios, folder templates and file scenario_list.html

--- scenarios/views.py
+++ scenarios/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Scenario
 
-# Create your views here.
+# def scenario_list(request):
+#     scenarios = Scenario.objects.all()
+#     return render(request, 'scenarios/scenario_list.html', {'scenarios': scenarios})
+
+def scenario_list(request):
+    scenarios = Scenario.objects.all()
+    return render(request, 'scenarios/scenario_list.html', {'scenarios': scenarios})
